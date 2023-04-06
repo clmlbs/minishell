b343da70bc1542bc1831940a6ad3b866b721c727
Clemence Leblais <cleblais@c2r1p5.42nice.fr>
2023-04-06 17:26:12 +0200

    export presque mais pas encore fini

--- builtin/export.c
+++ builtin/export.c
@@ -6,7 +6,7 @@
 /*   By: cleblais <cleblais@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/06 15:09:58 by cleblais          #+#    #+#             */
-/*   Updated: 2023/04/06 16:52:36 by cleblais         ###   ########.fr       */
+/*   Updated: 2023/04/06 17:20:11 by cleblais         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,8 +77,68 @@ void	export_without_args(void)
 	exit(SUCCESS);
 }
 
+void	export_var(char *str)
+{
+	char	**buf;
+
+	buf = copy_strs_plus_one(g_all.env);
+	if (!buf)
+		exit(perror_fail("Minishell: malloc()"));
+	buf[tab_strlen(g_all.env)] = ft_strdup(str);
+	if (!buf[tab_strlen(g_all.env)])
+		exit(perror_fail("Minishell: malloc()"));
+	free_tab_strs(g_all.env);
+	g_all.env = buf;
+	buf = NULL;
+}
+
+int	export_check_args(char **strs, int *i)
+{
+	int	j;
+	int equal_present;
+
+	j = 0;
+	equal_present = FAILURE;
+	if (strs[*i] && strs[*i][0] == '-')
+	{
+		write_error("Minishell: ", "options for export are not ", \
+			"implemented in this Minishell\n");
+		return (FAILURE);
+	}
+	else if (strs[*i] && is_spe_or_num(strs[*i][0]) == TRUE)
+	{
+		write_error("Minishell: ", "export:", " `");
+		ft_putstr_fd(strs[*i], STDERR_FILENO);
+		ft_putstr_fd("': not a valid identifier\n", STDERR_FILENO);
+		return (FAILURE);
+	}
+	while (strs[*i][j])
+	{
+		if (strs[*i][j] == '=')
+		{
+			printf("str:%s\n", strs[*i]);//*********
+			equal_present = SUCCESS;
+		}
+		j++;
+	}
+	return (equal_present);
+}
+
 void	execute_export(t_cmd *cmd)
 {
+	int		i;
+	char	**buf;
+// PENSER A VERIFIER LES CHIFFRES QUI SONT PAS VALIDES ET SI PAS DE = RIEN FAIRE 
+	i = 1;
 	if (!cmd->wd[1])
 		export_without_args();
+	while (cmd->wd[i])
+	{
+		if (export_check_args(cmd->wd, &i) == SUCCESS)
+			export_var(cmd->wd[i]);
+		i++;
+	}
+	if (send_env_to_father(g_all.env, g_all.herit) == FAILURE)
+		exit(FAILURE);
+	exit(SUCCESS);
 }
